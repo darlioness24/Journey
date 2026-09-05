56468c703b690cd9452c676363daff2aa20101dc
darlioness24 <kudryashova.dp@mail.ru>
2026-09-05 17:43:20 +0300

    Improve fetch event handling in service worker
    Enhance fetch event handling with cache fallback.

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
-  // Базовый Service Worker для работы PWA
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
